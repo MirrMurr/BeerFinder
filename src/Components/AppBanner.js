c329d67f5feab9f18047d822c5ed03e1523901b2
Adam Kara <kara.p.adam@gmail.com>
2020-08-12 21:37:39 +0200

    yesno.wtf/api call, basic username validation

--- src/Components/AppBanner.js
+++ src/Components/AppBanner.js
@@ -4,8 +4,8 @@ import '../Stylesheets/styles.css'
 export const AppBanner = () => {
   return (
     <div className="Banner">
-      <h3>Grape Solutions</h3>
-      <h3>Beer Finder®</h3>
+      <h3 className="Company">Grape Solutions</h3>
+      <h3 className="AppName">Beer Finder®</h3>
     </div>
   )
 }
--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -2,14 +2,45 @@ import React, { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
+// function useValidation (username) {
+//   const [isValidated, setIsValidated] = useState(false)
+//   useEffect(() => {
+//     fetch('https://yesno.wtf/api', {
+//       method: 'GET',
+//       headers: { Accept: 'application/json' }
+//     })
+//       .then(res => res.json())
+//       .then(response => {
+//         setIsValidated(response.answer === 'yes')
+//       })
+//   })
+//   return isValidated
+// }
+
 export const LoginPage = ({ LoginHandler }) => {
   const [username, setUsername] = useState('')
+  const [firstLoad, setFirstLoad] = useState(true)
+  const [isSubmitted, setIsSubmitted] = useState(false)
+  const [isValidated, setIsValidated] = useState(false)
   const history = useHistory()
+  // const isUserValidated = useValidation(username)
+
   let error = username === '' ? 'Username field must not be empty!' : null
   if (!error) {
     error = username.length > 16 ? 'Max. 16 characters.' : null
   }
-  const [firstLoad, setFirstLoad] = useState(false)
+
+  async function validateUsername (username) {
+    fetch('https://yesno.wtf/api', {
+      method: 'GET',
+      headers: { Accept: 'application/json' }
+    })
+      .then(res => res.json())
+      .then(response => {
+        console.log(response.answer)
+        setIsValidated(response.answer === 'yes')
+      })
+  }
 
   useEffect(() => {
     setFirstLoad(false)
@@ -20,9 +51,11 @@ export const LoginPage = ({ LoginHandler }) => {
   }, [])
 
   // TODO useEffect -> call yesno.wtf/api / useValidation own hook
-  const handleSubmit = (e) => {
+  function handleSubmit (e) {
     e.preventDefault()
-    if (!error) {
+    setIsSubmitted(true)
+    validateUsername(username)
+    if (!error && isValidated) {
       LoginHandler(username)
       history.push('/listing')
     }
@@ -39,7 +72,10 @@ export const LoginPage = ({ LoginHandler }) => {
         <input id="usernameInput" type="text" placeholder="Username" value={username} onChange={handleInputChange} />
         <button disabled={Boolean(error)} type="submit">Log in</button>
       </form>
-      <div id="error">{firstLoad ? null : error}</div>
+      <div className="error">{!firstLoad ? error : null}</div>
+      <div className="not-validated">
+        {(isValidated || firstLoad) && isSubmitted ? null : 'Your username has not been found eligible.'}
+      </div>
     </div>
   )
 }
