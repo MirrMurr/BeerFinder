35fa6aa6b4db0aa4ecd09c8c1a96ab50dac0bbca
Adam Kara <kara.p.adam@gmail.com>
2020-08-16 10:21:58 +0200

    removed comments

--- src/Components/Header/AppBanner.js
+++ src/Components/Header/AppBanner.js
@@ -5,9 +5,7 @@ import GrapeLogo from '../../grape_logo.svg'
 export const AppBanner = () => {
   return (
     <div className="Banner">
-      {/* <h3 className="Company">Grape Solutions</h3> */}
       <img className="Company" src={GrapeLogo} alt="grape logo" />
-      {/* <GrapeLogo fill="white" /> */}
       <h3 className="AppName">Beer Finder®</h3>
     </div>
   )
